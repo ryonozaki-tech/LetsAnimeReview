a64795b1ccca5f9482d93e8ab072f3e7bb3aee8d
ryonozaki-tech <komakoma95515@gmail.com>
2020-05-15 18:45:18 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,12 @@ describe User do
       expect(user.errors[:nickname]).to include("can't be blank")
     end
   end
+
+  describe '#create' do
+    it 'emailがない場合は登録できないこと' do
+      user = User.new(nickname: "test", email: "", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+  end
 end
