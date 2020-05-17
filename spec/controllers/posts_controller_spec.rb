4095354943a7377954fda72830f4b9aada9ab0cb
ryonozaki-tech <komakoma95515@gmail.com>
2020-05-17 12:08:51 +0900

    投稿機能単体テスト完了

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -23,7 +23,7 @@ describe PostsController do
   end
 
   describe 'GET #index' do
-    it "@postに正しい値が入っていること" do
+    it "@postsに正しい値が入っていること" do
       posts = create_list(:post, 3)
       get :index
       expect(assigns(:posts)).to match(posts.sort{ |a, b| b.created_at <=> a.created_at })
@@ -34,5 +34,78 @@ describe PostsController do
       expect(response).to render_template :index
     end
   end
-  
+
+  describe 'GET #star_ranking' do
+    it "@postsに正しい値が入っていること" do
+      posts = create_list(:post, 3)
+      get :star_ranking
+      expect(assigns(:posts)).to match(posts.sort{ |a, b| b.rate <=> a.rate })
+    end
+
+    it "star_ranking.html.hamlに遷移すること" do
+      get :star_ranking
+      expect(response).to render_template :star_ranking
+    end
+  end
+
+  describe 'GET #like_ranking' do
+    it "@postに正しい値が入っていること" do
+      posts = create_list(:post, 3)
+      get :like_ranking
+      expect(assigns(:posts)).to match(posts.sort{ |a, b| b.likes_count <=> a.likes_count })
+    end
+
+    it "like_ranking.html.hamlに遷移すること" do
+      get :like_ranking
+      expect(response).to render_template :like_ranking
+    end
+  end
+
+  describe 'GET #show' do
+    it "@postに正しい値が入っていること" do
+      post = create(:post)
+      get :show, params: { id: post }
+      expect(assigns(:post)).to eq post
+    end
+
+    it "show.html.hamlに遷移すること" do
+      post = create(:post)
+      get :show, params: { id: post }
+      expect(response).to render_template :show
+    end
+  end
+
+#   describe 'POST #create' do
+#     it "@postに正しい値が入っていること" do
+#       post = create(:post)
+#       # post :create
+#       expect(assigns(:post)).to eq post
+#     end
+#     it "index.html.hamlに遷移すること" do
+#       post = create(:post)
+#       # post :create
+#       expect(response).to render_template :index
+#     end
+#   end
+
+#   # describe 'PATCH #update' do
+#   #   it "@postに正しい値が入っていること" do
+#   #     post = create(:post)
+#   #     post = update(:post, nickname: "saki")
+#   #     patch :update, params: { id: post }
+#   #     expect(assigns(:post)).to eq post
+#   #   end
+#   # end
+
+#   describe 'DELETE #destroy' do
+#     it "postに値が入っていないこと" do
+#       post = create(:post)
+#       delete :destroy, params: { id: post }
+#       expect(assigns(:post)).to eq nil
+#     end
+#     it "削除し終えたらその前のビューに遷移すること" do
+#       delete :destroy
+#       expect(response).to render_template :back
+#     end
+#   end
 end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,11 +1,12 @@
 FactoryBot.define do
   
   factory :post do
-    title   {"ポケモン"}
-    review  {"かわいい"}
-    rate    {4}
-    image   {"https://pbs.twimg.com/profile_images/910863761795198977/hziHe73D_400x400.jpg"}
-    created_at { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
+    title       {"ポケモン"}
+    review      {"かわいい"}
+    image       {"https://pbs.twimg.com/profile_images/910863761795198977/hziHe73D_400x400.jpg"}
+    rate        { Faker::Number.between(from: 1, to: 5) }
+    created_at  { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
+    likes_count { Faker::Number.between(from: 0, to: 100) }
     user
   end
 
