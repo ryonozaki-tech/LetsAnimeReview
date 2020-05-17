aab6da350dea14214128ab6cdbe00c868aa4a8f9
ryonozaki-tech <komakoma95515@gmail.com>
2020-05-17 19:19:28 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Genre.create([{name: "恋愛"},
-              {name: "ギャグ"},
-              {name: "SF"},
-              {name: "ホラー"},
-              {name: "アクション"},
-              {name: "ミステリー"},
-              {name: "悲劇"},
-              {name: "歴史・未来"},
-              {name: "冒険"},
-              {name: "スポーツ"},
-              {name: "学園"},
-              {name: "青春"},
-              {name: "バトル"},
-              {name: "サバイバル"},
-              {name: "劇場版"}])
+# Genre.create([{name: "恋愛"},
+#               {name: "ギャグ"},
+#               {name: "SF"},
+#               {name: "ホラー"},
+#               {name: "アクション"},
+#               {name: "ミステリー"},
+#               {name: "悲劇"},
+#               {name: "歴史・未来"},
+#               {name: "冒険"},
+#               {name: "スポーツ"},
+#               {name: "学園"},
+#               {name: "青春"},
+#               {name: "バトル"},
+#               {name: "サバイバル"},
+#               {name: "劇場版"}])
 
 
-# Genre.create(name: "恋愛")
-# Genre.create(name: "ギャグ")
-# Genre.create(name: "SF")
-# Genre.create(name: "ホラー")
-# Genre.create(name: "アクション")
-# Genre.create(name: "ミステリー")
-# Genre.create(name: "悲劇")
-# Genre.create(name: "歴史・未来")
-# Genre.create(name: "冒険")
-# Genre.create(name: "スポーツ")
-# Genre.create(name: "学園")
-# Genre.create(name: "青春")
-# Genre.create(name: "バトル")
-# Genre.create(name: "サバイバル")
-# Genre.create(name: "劇場版")
+Genre.create(name: "恋愛")
+Genre.create(name: "ギャグ")
+Genre.create(name: "SF")
+Genre.create(name: "ホラー")
+Genre.create(name: "アクション")
+Genre.create(name: "ミステリー")
+Genre.create(name: "悲劇")
+Genre.create(name: "歴史・未来")
+Genre.create(name: "冒険")
+Genre.create(name: "スポーツ")
+Genre.create(name: "学園")
+Genre.create(name: "青春")
+Genre.create(name: "バトル")
+Genre.create(name: "サバイバル")
+Genre.create(name: "劇場版")
